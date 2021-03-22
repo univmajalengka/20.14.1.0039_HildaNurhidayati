c042756e9b10c1a730397d4f0d42f472b1dfafa9
HildaNurhidayati <73578366+HildaNurhidayati@users.noreply.github.com>
2021-03-22 23:34:43 +0700

    Add files via upload

---
+++ Tugas2/tugas2.1_menghitungluaspersegipanjang.pas
@@ -0,0 +1,21 @@
+{Nama           : Hilda Nurhidayati}
+{NPM            : 20.14.1.0039}
+{Nama Program   : Menghitung_Luas.pas}
+
+program luaspersegipanjang;
+uses crt;
+
+var
+panjang : integer;
+lebar   : integer;
+luas    : integer;
+
+begin
+write('Masukkan Panjang = 10  ');readln;
+write('Masukkan Lebar = 5 ');readln;
+
+Luas := panjang*lebar;
+write('Luas Persegi Panjang = 50');readln;
+
+end.
+
