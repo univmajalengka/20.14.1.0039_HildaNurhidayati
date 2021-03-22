88df6ef1c6f1fda900be79bf6b7860656a8a8bfd
HildaNurhidayati <73578366+HildaNurhidayati@users.noreply.github.com>
2021-03-22 23:06:17 +0700

    Add files via upload

---
+++ Tugas1/hello3.pas
@@ -0,0 +1,15 @@
+{Nama   : Hilda Nurhidayati}
+{NPM    : 20.14.1.0039}
+{Nama Program : hello3.pas}
+
+program hello3;
+uses crt;
+
+var
+nama : string;
+
+begin
+writeln('Contoh membaca dan menulis inputan');
+writeln('Masukkan Nama : ');readln(nama);
+
+end.
