ea820af091dbec56e2df4d4ec7ab4cb546d56f8b
HildaNurhidayati <73578366+HildaNurhidayati@users.noreply.github.com>
2021-04-01 00:03:26 +0700

    Add files via upload

---
+++ Tugas1/LatihanFPC/hello2.pas
@@ -0,0 +1,20 @@
+{Nama   : Hilda Nurhidayati}
+{NPM    : 20.14.1.0039}
+{Nama Program : hello2.pas}
+
+program hello2;
+uses crt;
+
+var
+i : integer;
+hello : string;
+
+begin
+hello := 'Hello,dunia';
+i := 1234;
+writeln(hello);
+writeln('Nilai i = ',1);
+
+readln;
+
+end.
